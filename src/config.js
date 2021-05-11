1b87e86e7ebdf7152f3f3db285ecedf777bbcbee
Kris Kettendorf <triggxl@hotmail.com>
2021-05-10 20:57:08 -0400

    added gitignore

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8002,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_URL: process.env.NODE_ENV === 'production' ? 'heroku url' : 'http://localhost:8002'
 }
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const { PORT } = require('../../src/config');
+const { PORT } = require('./config');
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
