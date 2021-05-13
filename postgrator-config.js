ba55aaf4343e292b7465205616001cbcf7adc0ce
Kris Kettendorf <triggxl@hotmail.com>
2021-05-12 20:18:29 -0400

    provisioned postgres, have access to DB on heroku, need to push data up, run migrations, seed files

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,7 +4,7 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
-    ? process.env.DATABASE_URL
-    : process.env.TEST_DATABASE_URL,
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 }
 console.log(process.env.DATABASE_URL)
