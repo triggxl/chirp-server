8d6b7b824d96ee78b89681c47e9b859af514535d
Kris Kettendorf <triggxl@hotmail.com>
2021-05-18 19:20:13 -0400

    tried importing dotenv in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-
+// import dotenv from 'dotenv'
+// dotenv.config()
 module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL || { host: 'localhost', user: 'Triggxl', password: '', database: 'chirp-app' },
